1f52d286955309921e0eb5eb3f1c16fab20de42c
MF-swiss <marco.fritsche@outlook.com>
2026-07-04 11:54:04 +0200

    ordnerstruktur

--- backend/src/main/java/ch/swiss/eventbackend/EventsbackendApplication.java
+++ backend/src/main/java/ch/swiss/eventbackend/EventsbackendApplication.java
@@ -1,5 +1,12 @@
 package ch.swiss.eventbackend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EventsbackendApplication {
-    
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsbackendApplication.class, args);
+    }
 }
